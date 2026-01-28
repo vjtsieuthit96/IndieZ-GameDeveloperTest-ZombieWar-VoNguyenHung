85097e45ebbbca1bb266d7294a7e6ab39dec0155
vjtsieuthit96 <vonguyenhung96@gmail.com>
2026-01-28 23:51:25 +0700

    Dissolve Shader

--- Assets/Scripts/Enemy/ZombieStats.cs
+++ Assets/Scripts/Enemy/ZombieStats.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using System.Collections;
 
 public class ZombieStats : MonoBehaviour
 {
     [SerializeField] private EnemyDataSO enemyData;
-
     [SerializeField] private float currentHP;
+    [SerializeField] private SkinnedMeshRenderer[] skinnedMeshes;
     private bool isDead = false;
 
-    private void Start()
-    {
+    private void OnEnable()
+    {        
         currentHP = enemyData.maxHP;
+        isDead = false;
+        
+        foreach (var smr in skinnedMeshes)
+        {
+            smr.material.SetFloat("_DissolveStrength", 0);           
+        }
+    }
+
+    private void Update()
+    {
+        if (currentHP <= 0 && !isDead)
+        {
+            Die();
+        }
     }
 
     public void TakeDamage(float amount)
@@ -24,20 +39,30 @@ public class ZombieStats : MonoBehaviour
         }
     }
 
-    public float GetDamage()
-    {
-        return enemyData.damage;
-    }
-
-    public float GetMoveSpeed()
-    {
-        return enemyData.moveSpeed;
-    }
+    public float GetDamage() => enemyData.damage;
+    public float GetMoveSpeed() => enemyData.moveSpeed;
 
     private void Die()
     {
         if (isDead) return;
-        isDead = true;           
-        ObjectPoolManager.ReturnObjectToPool(this.gameObject);
+        isDead = true;
+        StartCoroutine(DissolveAndReturn());
+    }
+
+    private IEnumerator DissolveAndReturn()
+    {
+        float amount = 0;
+        while (amount < 1)
+        {
+            amount += Time.deltaTime * 0.4f;
+            foreach (var smr in skinnedMeshes)
+            {
+                Material mat = smr.material;
+                smr.material.SetFloat("_DissolveStrength", amount);
+            }
+            yield return null;
+        }
+
+        //ObjectPoolManager.ReturnObjectToPool(this.gameObject);
     }
 }
