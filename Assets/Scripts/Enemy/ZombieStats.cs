b0e3e686f8b440b8418ec0c51ac3c123efd8e93b
vjtsieuthit96 <vonguyenhung96@gmail.com>
2026-01-29 01:28:41 +0700

    Enemy take damage

--- Assets/Scripts/Enemy/ZombieStats.cs
+++ Assets/Scripts/Enemy/ZombieStats.cs
@@ -8,14 +8,17 @@ public class ZombieStats : MonoBehaviour
     [SerializeField] private SkinnedMeshRenderer[] skinnedMeshes;
     private bool isDead = false;
 
+    private Coroutine hitEffectCoroutine; 
+
     private void OnEnable()
-    {        
+    {
         currentHP = enemyData.maxHP;
         isDead = false;
-        
+
         foreach (var smr in skinnedMeshes)
         {
-            smr.material.SetFloat("_DissolveStrength", 0);           
+            smr.material.SetFloat("_DissolveStrength", 0);
+            smr.material.SetFloat("_HitEffect", 0); 
         }
     }
 
@@ -33,6 +36,15 @@ public class ZombieStats : MonoBehaviour
 
         currentHP -= amount;
         Debug.Log($"Zombie took {amount} damage. Current HP: {currentHP}");
+
+        
+        if (hitEffectCoroutine != null)
+        {
+            StopCoroutine(hitEffectCoroutine);
+        }
+        
+        hitEffectCoroutine = StartCoroutine(HitEffectRoutine());
+
         if (currentHP <= 0)
         {
             Die();
@@ -45,24 +57,49 @@ public class ZombieStats : MonoBehaviour
     private void Die()
     {
         if (isDead) return;
+        GameEventManager.Instance.InvokeEnemyDie(this.gameObject);
         isDead = true;
         StartCoroutine(DissolveAndReturn());
     }
 
+    private IEnumerator HitEffectRoutine()
+    {
+        float timer = 0f;
+        float duration = 5f;
+
+        while (timer < duration)
+        {
+            float value = Mathf.PingPong(Time.time * 4f, 0.75f);
+            foreach (var smr in skinnedMeshes)
+            {
+                smr.material.SetFloat("_HitEffect", value);
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        foreach (var smr in skinnedMeshes)
+        {
+            smr.material.SetFloat("_HitEffect", 0);
+        }
+
+        hitEffectCoroutine = null;
+    }
     private IEnumerator DissolveAndReturn()
     {
         float amount = 0;
         while (amount < 1)
         {
-            amount += Time.deltaTime * 0.4f;
+            amount += Time.deltaTime * 0.25f;
             foreach (var smr in skinnedMeshes)
             {
-                Material mat = smr.material;
-                smr.material.SetFloat("_DissolveStrength", amount);
+                smr.material.SetFloat("_DissolveStrength", amount);                
             }
             yield return null;
         }
 
         //ObjectPoolManager.ReturnObjectToPool(this.gameObject);
     }
+    
 }
